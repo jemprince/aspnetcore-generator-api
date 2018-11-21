a5e195bc7c9d511cb436f9b5715b6d6c671d12ca
jem <jem.prince@ntlworld.com>
2018-11-21 15:12:50 +0000

    Add tests

---
+++ mstests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace mstests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
