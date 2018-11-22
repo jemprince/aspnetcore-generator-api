2ff4e4db42ac7185e1aa42bd412a797efca37cd4
jem <jem.prince@ntlworld.com>
2018-11-22 10:01:32 +0000

    add test

--- tests/RangeTests.cs
+++ tests/RangeTests.cs
@@ -15,5 +15,14 @@ namespace tests
             var generated = range.Of(() => "");
             Assert.Equal(3, generated.Count());
         }
+        [Fact]
+        public void SortShouldOrderResults()
+        {
+            var range = new Range { Count = 3 };
+            var values = new[] { "a", "b", "c" };
+            var counter = 0;
+            var generated = range.Of(() => values[counter++]);
+            Assert.Equal(new[] { "a", "b", "c" }, generated.ToArray());
+        }
     }
 }
